e5e999dad311b30ef816cfc875f7e49237b008f1
IsHaltEchtSo <d.grollmusz@gmail.com>
2022-07-20 18:03:55 +0200

    different input types

--- marshmallow_plain/quickstart.py
+++ marshmallow_plain/quickstart.py
@@ -3,10 +3,12 @@ LEARNINGS
 serialize objects to dictionaries, choose which attributes to include/exclude
 deserialize objects into dictionaries or objects with @post_load
 serialize many objects into a nested dictionary
+objects to be serialized can be of different types
+marshmallow only serializes the fields specified; additional ones are omitted, missing ones are left out
 """
 
 
-# LANDING PAGE
+# FROM THE LANDING PAGE
 from datetime import date
 from pprint import pprint
 from marshmallow import Schema, fields
@@ -38,7 +40,7 @@ import datetime as dt
 from marshmallow import Schema, fields
 
 class User:
-    def __init__(self, name, email, **kwargs) -> None:
+    def __init__(self, name: str, email: str, **kwargs) -> None:
         self.name = name
         self.email = email
         self.created_at = dt.datetime.now()
@@ -105,7 +107,7 @@ class UserSchema(Schema):
     created_at = fields.DateTime()
 
     @post_load
-    def make_user(self, data, **kwargs):
+    def make_user(self, data, **kwargs) -> User:
         return User(**data)
 
 user_schema = UserSchema()
@@ -120,3 +122,33 @@ luca = User(name='luca', email='luca@beach.org')
 user_schema = UserSchema()
 user_serialized = user_schema.dump(obj=[deniz, luca], many=True)
 pprint(user_serialized, indent=2)
+
+
+
+# FROM OBJ TO DICT
+class Hardware:
+    def __init__(self, name: str, type: str, price: float) -> None:
+        self.name = name
+        self.type = type
+        self.price = price  # THIS WON'T BE EXTRACTED BY THE HARDWARE SCHEMA
+
+    def __repr__(self) -> str:
+        return f"<{self.type} {self.name}>"
+
+class HardwareSchema(Schema):
+    """
+    Given some input object/dict, the schema only extracts the values for keys that are identical to its attributes
+    """
+    name = fields.Str()
+    type = fields.Str()
+
+    @post_load
+    def make_hardware(self, data, **kwargs) -> Hardware:
+        return Hardware(**data)
+
+iPhone = Hardware(name='iPhone', type='Smartphone', price=649.50)
+magic_mouse = dict(type='Bluetooth Mouse', price=114.90, alias='super mouse')
+
+hardware_schema = HardwareSchema()
+hardware_serialized = hardware_schema.dump(obj=[iPhone, magic_mouse], many=True)
+pprint(hardware_serialized, indent=2)
